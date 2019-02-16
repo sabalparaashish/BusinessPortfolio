a984f1a9a8f1c980adf41dd64820347bdcfe5d33
sabalparaashish <sabalparaashish.as@gmail.com>
2019-02-16 17:53:24 -0500

    Added All Pages and Routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'contactme', component: ContactmeComponent},
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'services', component: ServicesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    ServicesComponent,
+    ContactmeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
