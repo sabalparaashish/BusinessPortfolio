de32cba466627cd1b0ce0aa8e9fad3e65cc81535
sabalparaashish <sabalparaashish.as@gmail.com>
2019-02-16 23:15:38 -0500

    added all pages content

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contactme',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactmeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
   }
-
 }
